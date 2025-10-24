c8b0b4781a7accd71336d11ccb6574f1c19040b4
MasterBuilderP <239988859+MasterBuilderP@users.noreply.github.com>
2025-10-24 16:10:34 +0200

    cleanup reduntant code. improve calendar source logic

--- kcal/calendar.py
+++ kcal/calendar.py
@@ -1,9 +1,20 @@
 from .cache import cache
-from .cal_sources.evolution import get_events
+from .cal_sources.evolution import get_events as get_events_evolution
+
+sources = [get_events_evolution]
 
 
 def main():
-    existing = len(cache())
-    events = len(get_events())
-    if events > existing:
-        print(f"Found {events - existing} new events")
+    existing = cache().keys()
+    data = {}
+    for source in sources:
+        data.update(source())
+    all = cache(data).keys()
+
+    new = all - existing
+    if len(new) > 0:
+        print(f"Found {len(new)} new events")
+
+    cleaned = existing - all
+    if len(cleaned) > 0:
+        print(f"Cleaned up {len(cleaned)} events")
